ea880f2402b7b305d56e453656d3ca0ffc6c323f
MuzzammilAhmedKhan <muzzammilahmedk1@gmail.com>
2022-10-15 00:21:14 +0530

    Simulate a match of 5 rounds
    with some yet to be resolved errors

--- script.js
+++ script.js
@@ -1,46 +1,76 @@
-function getComputerChoice() {
-  const choice = ["rock", "paper", "scissors"];
-  return choice[Math.floor(Math.random() * choice.length)];
-}
+//DOM
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorBtn = document.querySelector(".scissor");
+const scorePlayer = document.querySelector('.player-score');
+const scoreComputer = document.querySelector('.computer-score');
+const resultDisplay = document.querySelector('.result-text');
 
-function playRound(playerSelection, computerChoice) {
+//EVENTS
+let rockPick = rockBtn.addEventListener("click", () => {
+  playOneGame(items[0]);
+});
+
+let paperPick = paperBtn.addEventListener('click', () => {
+  playOneGame(items[1]);
+});
+
+let scissorPick = scissorBtn.addEventListener('click', () => {
+  playOneGame(items[2]);
+})
+
+//game logic
+const items = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+let result;
+
+let getComputerChoice = () => {
+  return items[Math.floor(Math.random() * items.length)];
+};
+
+let playOneRound = (playerChoice, computerChoice) => {
   let computerSelection = computerChoice();
-  playerSelection = playerSelection.toLowerCase();
+  playerSelection = playerChoice.toLowerCase();
+  let paper = "Paper beats rock";
+  let rock = "Rock beats scissors";
+  let scissors = "Scissors beat paper";
+  let win = "You win!";
+  let lose = "You lose!";
   if (computerSelection === playerSelection) {
-    return 0;
+    result = `It's a draw! ${playerSelection} = ${computerSelection}`;
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return -1;
+    result = `${lose} ${paper}.`;
+    computerScore++;
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return -1;
+    result = `${lose} ${scissors}.`;
+    computerScore++;
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return -1;
+    result = `${lose} ${rock}.`;
+    computerScore++;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return 1;
+    result = `${win} ${paper}`;
+    playerScore++;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return 1;
+    result = `${win} ${scissors}`;
+    playerScore++;
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return 1;
+    result = `${win} ${rock}`;
+    playerScore++;
   } else {
-    return 0;
+    result = `Error! Selection not in choice. ${playerSelection} and ${computerSelection}`;
   }
-}
+  return result;
+};
 
-var playerScore = document.querySelector(".player-score").innerHTML;
-const computerScore = document.querySelector(".computer-score").innerHTML;
+let playOneGame = (playerChoice) => {
+  playOneRound(playerChoice, getComputerChoice);
+  scoreComputer.textContent = `${computerScore}`;
+  scorePlayer.textContent = `${playerScore}`;
 
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissor = document.querySelector(".scissor");
-
-let score 
-
-for (let i = 0; i < 5; i++) {
-  rock.addEventListener("click", () => {
-    score = setTimeout(playRound("rock", getComputerChoice), 5000);
-    // console.log(score)
-  });
-}
-
-console.log(score); // <--- always returns undefined
-
-
+  if(playerScore < 5 && computerScore === 3) {
+    resultDisplay.textContent = `You Lost! `;
+  } else {
+    resultDisplay.textContent = `You Won! `;
+  }
+};
